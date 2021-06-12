4682882a99e7051a01c8005ef8436b8b146b6028
ljh <leejh95@nate.com>
2021-06-12 16:44:15 +0900

    Add Pagination Component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Page, PageContainer, PageWrapper } from './styles/Pagination';
 
 type Props = {
@@ -5,11 +6,18 @@ type Props = {
 };
 
 const Pagination = ({ countPage }: Props) => {
+  const [clicked, setClicked] = useState<number | null>(0);
   return (
     <PageContainer>
       <PageWrapper>
         {Array.from({ length: countPage }, (_, i) => (
-          <Page to={`/game?page=${i + 1}`}>{i + 1}</Page>
+          <Page
+            to={`/game?page=${i + 1}`}
+            onClick={() => setClicked(i)}
+            clicked={clicked === i ? 1 : 0}
+          >
+            {i + 1}
+          </Page>
         ))}
       </PageWrapper>
     </PageContainer>
--- src/components/Pagination/styles/Pagination.ts
+++ src/components/Pagination/styles/Pagination.ts
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+type Props = {
+  clicked: number;
+};
+
 export const PageContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -12,7 +16,7 @@ export const PageWrapper = styled.div``;
 
 export const Page = styled(Link)`
   text-decoration: none;
-  color: #aaaaaa;
   padding: 18px;
   font-size: 28px;
+  color: ${({ clicked }: Props) => (clicked ? '#111' : '#aaaaaa')};
 `;
